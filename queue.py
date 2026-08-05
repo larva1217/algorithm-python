caa17dda4580eccf98de5fc9de3d4b7627ca3e89
larva1217 <larva1217@gmail.com>
2026-08-05 17:10:01 +0900

    queue

---
+++ queue.py
@@ -0,0 +1,17 @@
+#큐
+#먼저 들어온 데이터가 먼저 출력
+
+from collections import deque
+
+queue=deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+print(queue)
+
+queue.popleft()
+print(queue)
+
+queue.popleft()
+print(queue)
